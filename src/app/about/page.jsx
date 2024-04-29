99eb8d2907b16218a5fd594719a5784e8004fb4b
Salam Aziz <azizsalam148@gmail.com>
2024-04-29 18:15:11 +0000

    added an about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        assumenda, nulla temporibus itaque maxime possimus sunt eum eaque vero
+        nisi aspernatur, eveniet asperiores dolores exercitationem!
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+        perspiciatis dolorem optio, illo ipsam placeat minima voluptate
+        exercitationem error itaque eum repellat harum fugit corrupti iure
+        possimus maiores consequatur facere culpa consequuntur vel molestiae
+        doloribus? Harum nisi voluptate repudiandae adipisci iure quaerat at
+        facilis blanditiis illum magnam magni animi quo, omnis numquam odio cum
+        sunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+        perspiciatis dolorem optio, illo ipsam placeat minima voluptate
+        exercitationem error itaque eum repellat harum fugit corrupti iure
+        possimus maiores consequatur facere culpa consequuntur vel molestiae
+        doloribus? Harum nisi voluptate repudiandae adipisci iure quaerat at
+        facilis blanditiis illum magnam magni animi quo, omnis numquam odio cum
+        sunt!
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo
+        error eveniet nam molestiae veniam officiis quae iste. Esse, adipisci
+        ratione deserunt, sapiente, sed perferendis corrupti libero facilis
+        provident iure quisquam neque odio repudiandae commodi similique veniam
+        modi voluptate id dolorem enim consequatur cum! Fugiat consequuntur ea
+        eligendi quidem dolores tempore magnam cumque asperiores nulla!
+      </p>
+    </div>
+  );
+}
